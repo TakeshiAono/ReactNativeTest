7e38226ed27f9c6b13e1d18ff3b2189b34662456
aonot <aonot@solutionware.jp>
2024-01-31 22:41:01 +0900

    データベース名変更

--- hooks/useSQlite.tsx
+++ hooks/useSQlite.tsx
@@ -1,6 +1,6 @@
 import * as SQLite from 'expo-sqlite';
 
 export default function useSQlite() {
-  const db = SQLite.openDatabase("blogDB14");
+  const db = SQLite.openDatabase("blogTest");
   return db
 }
