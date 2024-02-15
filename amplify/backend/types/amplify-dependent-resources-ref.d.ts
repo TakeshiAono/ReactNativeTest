531010c6fd03dad79eeb10b57e733ec19a3cfd2c
aonot <aonot@solutionware.jp>
2024-02-16 00:48:13 +0900

    amplify環境のセッティング

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "blogbackend": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
